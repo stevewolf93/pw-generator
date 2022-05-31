a7216732306eec277dfad7350c93bd0aaf543d00
Steve <stevewolf93@gmail.com>
2022-05-31 15:06:14 +0200

    basic functuality done

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+
+
+const label = document.querySelector(`#showPassword`);
 function randomString() {
-  let randomString = Math.random().toString(36).slice(2,16)
-return randomString
+  let randomString = Math.random().toString(36).slice(2, 16);
+  console.log(randomString);
+label.innerHTML = randomString;
+  return randomString;
 }
